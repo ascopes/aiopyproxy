2470b5e649e6f70ac9c1a3d993ec43560c46b264
ascopes <73482956+ascopes@users.noreply.github.com>
2023-07-23 08:45:58 +0100

    Update __init__.py imports

--- aiopyproxy/__init__.py
+++ aiopyproxy/__init__.py
@@ -2,11 +2,5 @@
 """Aiopyproxy - a simple HTTP proxy in Python."""
 from __future__ import annotations
 
-from typing import Final, Sequence
-
-from . import _meta
-
-__author__ = _meta.author
-__version__ = _meta.version
-
-__all__: Final[Sequence[str]] = ()
+from aiopyproxy._meta import __author__
+from aiopyproxy._meta import __version__
