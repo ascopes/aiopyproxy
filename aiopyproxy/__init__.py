6a8cdba3ceb182716493b8cef02193fc0ff33b7b
ascopes <73482956+ascopes@users.noreply.github.com>
2023-07-23 08:46:12 +0100

    Update __init__.py

--- aiopyproxy/__init__.py
+++ aiopyproxy/__init__.py
@@ -2,5 +2,5 @@
 """Aiopyproxy - a simple HTTP proxy in Python."""
 from __future__ import annotations
 
-from aiopyproxy._meta import __author__
-from aiopyproxy._meta import __version__
+from aiopyproxy._meta import author as __author__
+from aiopyproxy._meta import version as __version__
